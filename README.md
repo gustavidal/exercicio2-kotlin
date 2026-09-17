0fdc13853bb9135d3f5d116baf05a3b86e4544d5
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246733.cfp127aluno.local>
2026-09-17 16:47:17 -0300

    feat: enviar tela principal

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# exercicio2-kotlin
+# Space Invaders
--- app/src/main/java/com/example/spaceinvaders/MainActivity.kt
+++ app/src/main/java/com/example/spaceinvaders/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.spaceinvaders.ui.theme.SpaceInvadersTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +37,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             SpaceInvadersTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    BasicComponentScreen(
+//                        modifier = Modifier
+//                            .padding(innerPadding)
+//                    )
                 }
             }
         }
@@ -31,17 +48,138 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun BasicComponentScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp, 50.dp, 5.dp, 15.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "SCORE: 0050",
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White
+                )
+
+
+                Row (
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "LIVES: ",
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White,
+                        modifier = Modifier
+                            .padding(5.dp, 0.dp)
+                    )
+                    LifeHeart(
+                        modifier = Modifier
+                            .size(25.dp)
+                    )
+                    LifeHeart(
+                        modifier = Modifier
+                            .size(25.dp)
+                    )
+                    LifeHeart(
+                        modifier = Modifier
+                            .size(25.dp)
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp, 0.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                AndroidEnemy(
+                    modifier = Modifier
+                        .size(70.dp),
+                    color = Color.Green
+                )
+                AndroidEnemy(
+                    modifier = Modifier
+                        .size(70.dp),
+                    color = Color.Red
+                )
+                AndroidEnemy(
+                    modifier = Modifier
+                        .size(70.dp),
+                    color = Color.Blue
+                )
+                AndroidEnemy(
+                    modifier = Modifier
+                        .size(70.dp),
+                    color = Color.Yellow
+                )
+                AndroidEnemy(
+                    modifier = Modifier
+                        .size(70.dp),
+                    color = Color.Magenta
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(100.dp),
+                painter = painterResource(R.drawable.cube),
+                contentDescription = "Colored Cube"
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .background(Color.DarkGray),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "PRESS START",
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GameOverScreen(modifier: Modifier = Modifier) {
+    Box() {
+
+    }
+}
+
+@Composable
+fun LifeHeart(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.life_heart),
+        contentDescription = "LifeHeart"
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SpaceInvadersTheme {
-        Greeting("Android")
-    }
+fun AndroidEnemy(
+    modifier: Modifier = Modifier,
+    color: Color
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.ic_android_robot),
+        colorFilter = ColorFilter.tint(color),
+        contentDescription = "Android Enemy"
+    )
 }
